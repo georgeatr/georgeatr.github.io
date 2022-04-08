160be07db408b659af88cd2f685fff9cda3b23e7
georgeatr <georgeatr97@gmail.com>
2022-04-08 18:18:02 -0400

    Functions for project section
    Added Functions on projects.html
    A few style changes for projects.html

---
+++ js/functions.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+
+    $("#mainGame").click(function(){
+        window.open("https://jorgeatr.itch.io/leaf-lad-polluted-lands");
+    });
+
+    $("#game2").click(function(){
+        window.open("https://jorgeatr.itch.io/remember-me");
+    });
+
+    $("#game3").click(function(){
+        window.open("https://jorgeatr.itch.io/project-nano");
+    });
+
+    $("#game4").click(function(){
+        window.open("https://jorgeatr.itch.io/run-agarry-run");
+    });
+
+});
